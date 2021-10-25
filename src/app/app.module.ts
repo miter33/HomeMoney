cf8e752a4997ab1c19107988c387aba170912e03
Aliaksei Huivan <a.huivan@godeltech.com>
2021-10-26 01:01:53 +0300

    add auth components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
+import {AuthModule} from "./auth/auth.module";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import {FormsModule} from "@angular/forms";
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
